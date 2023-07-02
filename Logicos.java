6cc1e5f3a85d28ad6ef8c25f28862bc16060adf1
Felipe da Silva <felipe.silva.pt@outlook.pt>
2023-07-03 00:10:08 +0100

    Initial commit

---
+++ Logicos.java
@@ -0,0 +1,21 @@
+package Fundamentos;
+
+public class Logicos {
+	public static void main(String[] args) {
+		
+		boolean valor = true;
+		boolean valor2 = 3 > 7;
+		
+		System.out.println(valor);
+		System.out.println(valor2);
+		
+		
+		System.out.println(valor && valor2); //AND 
+		System.out.println(valor || valor2); //OR 
+		System.out.println(valor ^ valor2); //OU exclusivo "XOR"
+		System.out.println(!valor); //NOT, Negação 
+		
+		
+	}
+
+}
