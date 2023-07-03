ebe7d6622389022ae7f75bdf16474a72687bbc5a
Felipe da Silva <felipe.silva.pt@outlook.pt>
2023-07-04 00:28:30 +0100

    Operadores Unarios
    Nada de segredos, caso usar o operador ++ irei estar adicionando +1 ao valor atribuido a variavel. Da mesma forma que se usar o -- irei subtrair.
    
    Uma observação, se eu coloco o operador a frente da variavel, por exemplo ++A, quando eu executar, a leitura sera feito primeiro por ele, pois é colocado em prioridade.

--- OperadoresRelacionais.java
+++ OperadoresRelacionais.java
@@ -3,8 +3,8 @@ package Fundamentos;
 public class OperadoresRelacionais {
 		public static void main(String[] args) {
 			
-			System.out.println( 3 == 3 );
-			System.out.println( 7 < 6);
+			System.out.println(3 == 3);
+			System.out.println(7 < 6);
 			System.out.println(7 > 6);
 			System.out.println(4 >= 5);
 			System.out.println(3 <= 3);
